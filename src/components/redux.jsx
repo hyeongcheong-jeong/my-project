00af71cfcf17894fbbfb3d7f0fb3062924e2c311
hyeongcheol-jeong <105179763+hyeongcheong-jeong@users.noreply.github.com>
2024-11-20 09:48:52 +0900

    redux관련 추가 설명

--- src/components/redux.jsx
+++ src/components/redux.jsx
@@ -86,6 +86,51 @@ export default function Redux() {
     )
   }
   `;
+  const combineReducers = `  
+  import { createStore } from "redux";
+  import rootReducer from "../reducer";
+
+  const store = createStore(rootReducer);
+
+  export default store;
+  `;
+  const combineReducers1 = `  
+  import { combineReducers } from "redux";
+  import userReducer from "./userReducer";
+  
+  //rootReducer에 combineReducers를 사용하여 userReducer 통합
+  const rootReducer = combineReducers({
+    userInfo: userReducer,
+  });
+
+  export default rootReducer;
+  `;
+  const combineReducers2 = `  
+  //리덕스 초기 상태값
+  const initialState = {
+    name: "정형철",
+    age: 40,
+    job: "frontEngineer",
+  };
+
+  //switch 또는 if문으로 action type에 대한 reducer 작성
+  const userReducer = (state, action) => {
+    console.log(state, action);
+    switch (action.type) {
+      case "ACTION_LEE":
+        return {
+          ...state,
+          name: "이동욱",
+          age: 35,
+          job: "backEngineer",
+        };
+      default:
+        return initialState;
+    }
+  };
+
+  export default userReducer;
+  `;
   return (
     <section>
       <h1>react-redux jsx</h1>
@@ -143,6 +188,31 @@ export default function Redux() {
           <h3>useDispatch 결과값</h3>
           <Dispath />
           <UserInfo />
+        </dd>
+      </dl>
+      <dl>
+        <dt>6.combineReducers</dt>
+        <dd>
+          <p>
+            reducer가 개수가 늘어날 경우 rootReducer에 모두 정의하기에는 소스가
+            많이 복잡해 지고 유지 보수가 힘들어지는 면이 있다.
+          </p>
+          <p>
+            이럴경우 reducer를 별도 파일로 분리하여 작성하고 combineReducers를
+            사용하여 분할된 reducer를 통합할 수 있다.
+          </p>
+          <pre>
+            <p className="code-header">./reducer/userReducer.js</p>
+            {combineReducers2}
+          </pre>
+          <pre>
+            <p className="code-header">./reducer/index.js</p>
+            {combineReducers1}
+          </pre>
+          <pre>
+            <p className="code-header">./store/index.js</p>
+            {combineReducers}
+          </pre>
         </dd>
       </dl>
     </section>
